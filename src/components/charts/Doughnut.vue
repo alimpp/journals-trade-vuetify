1a68c18bdba3c736f396a357f37d07a911784f55
alimpp <hmdmpp@gmail.com>
2024-06-06 00:50:24 -0800

    Add chart

---
+++ src/components/charts/Doughnut.vue
@@ -0,0 +1,24 @@
+<template>
+  <Doughnut :data="data" :options="options" />
+</template>
+
+<script setup>
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "vue-chartjs";
+import { ref } from "vue";
+import { defineProps } from "vue";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const props = defineProps({
+  data: {
+    type: Object,
+    default: {},
+  },
+});
+
+const options = ref({
+  responsive: true,
+  maintainAspectRatio: false,
+});
+</script>
--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -1,47 +1,62 @@
 <template>
-  <div
-    class="app_overflow_hidden app-flex app-flex-column app-container app-h-90 app-w-100"
-  >
-    <BaseDivider content="APPLICATION THEME" width="170px" />
-    <div class="app-flex app-mt-5 app-mb-5">
-      <div
-        class="app-flex app_pointer fade_animations"
-        @click="HandleChangeTheme"
-      >
-        <v-icon class="app_pointer">mdi-weather-night</v-icon>
-        <span class="app-font-size-16 app-px-2">Dark</span>
+  <div class="app-flex app-container app-w-100 app-h-90 slid-left-animation">
+    <div class="app-flex app-flex-column app-w-50 app-mt-15">
+      <BaseDivider content="APPLICATION THEME" width="170px" />
+      <div class="app-flex app-mt-5 app-mb-5">
+        <div
+          class="app-flex app_pointer fade_animations"
+          @click="HandleChangeTheme"
+        >
+          <v-icon class="app_pointer">mdi-weather-night</v-icon>
+          <span class="app-font-size-16 app-px-2">Dark</span>
+        </div>
+        <div
+          class="app-flex app-mx-5 app_pointer fade_animations"
+          @click="HandleChangeTheme"
+        >
+          <v-icon class="app_pointer">mdi-white-balance-sunny</v-icon>
+          <span class="app-font-size-16 app-px-2">Dark</span>
+        </div>
       </div>
-      <div
-        class="app-flex app-mx-5 app_pointer fade_animations"
-        @click="HandleChangeTheme"
-      >
-        <v-icon class="app_pointer">mdi-white-balance-sunny</v-icon>
-        <span class="app-font-size-16 app-px-2">Dark</span>
+      <BaseDivider content="USER PROFILE" width="125px" />
+      <div class="app-flex app-mt-5">
+        <div class="app-w-300">
+          <BaseInput
+            v-model="form.username"
+            label="Username"
+            :error="validate.username.state"
+            :messageError="validate.username.text"
+          />
+        </div>
+        <div class="app-w-300 app-mx-2">
+          <BaseInput v-model="form.email" label="Email" :disabled="true" />
+        </div>
       </div>
+      <BaseButton
+        class="app-mt-3"
+        name="Edit Profile"
+        width="110px"
+        height="35px"
+        :loading="loading"
+        @click="editProfile"
+        tooltip="Edit Your Profile"
+      />
     </div>
-    <BaseDivider content="USER PROFILE" width="125px" />
-    <div class="app-flex app-mt-5">
-      <div class="app-w-300">
-        <BaseInput
-          v-model="form.username"
-          label="Username"
-          :error="validate.username.state"
-          :messageError="validate.username.text"
-        />
-      </div>
-      <div class="app-w-300 app-mx-2">
-        <BaseInput v-model="form.email" label="Email" :disabled="true" />
-      </div>
+    <div class="app-flex app-flex-column app-w-50 app-mt-15">
+      <BaseDivider content="ABOUT JOURNAL TRADE PROFILE" width="270px" />
+      <span class="app-font-size-18 app-font-weight-100 app-pt-5"
+        >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem
+        blanditiis animi quia architecto ut laborum numquam repudiandae nemo
+        molestiae! Excepturi esse cupiditate debitis, deserunt assumenda
+        inventore laudantium ut provident velit Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Vitae assumenda deleniti vel et voluptate
+        saepe dolorem illum magnam, ullam consequuntur eum fugiat possimus optio
+        quas distinctio fuga magni numquam velit! Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Doloribus reprehenderit consectetur omnis
+        quam id ullam accusantium totam, nihil, pariatur minus accusamus
+        maiores. Officiis deserunt nisi ipsa, optio dolor quas pariatur?
+      </span>
     </div>
-    <BaseButton
-      class="app-mt-3"
-      name="Edit Profile"
-      width="110px"
-      height="35px"
-      :loading="loading"
-      @click="editProfile"
-      tooltip="Edit Your Profile"
-    />
   </div>
 </template>
 
@@ -53,6 +68,7 @@ import { validateUsername } from "@/utils/validatie.js";
 import BaseInput from "@/components/base/baseInput.vue";
 import BaseButton from "@/components/base/baseButton.vue";
 import BaseDivider from "@/components/base/baseDivider.vue";
+import Doughnut from "@/components/charts/Doughnut.vue";
 
 const profileDS = profileDataStore();
 const themeDS = themeDataStore();
@@ -60,6 +76,16 @@ const loading = ref(false);
 
 const profileInformation = computed(() => {
   return profileDS.user;
+});
+
+const data = ref({
+  labels: ["Total Positions", "Targets", "Stop Loss"],
+  datasets: [
+    {
+      backgroundColor: ["#7d70f1", "#59bc1b", "#ff3333"],
+      data: [40, 20, 80],
+    },
+  ],
 });
 
 const form = ref({
@@ -103,3 +129,10 @@ onMounted(() => {
   form.value.email = profileInformation.value.email;
 });
 </script>
+
+<style scoped>
+.chart-content {
+  width: 400px;
+  height: 400px;
+}
+</style>
