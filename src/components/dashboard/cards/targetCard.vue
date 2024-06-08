525f562cb61a98e897c1abb64b605be99d540e21
alimpp <hmdmpp@gmail.com>
2024-06-08 03:48:31 -0800

    Create dashboard card

---
+++ src/components/dashboard/cards/targetCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="app-w-30 app_border_radius app-py-10 app-px-4 app-flex app-flex-column"
+  >
+    <span>{{ title }}</span>
+    <span>{{ date }}</span>
+    <span>{{ count }}</span>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+const title = computed(() => {
+  return;
+});
+
+const date = computed(() => {
+  return;
+});
+
+const count = computed(() => {
+  return;
+});
+</script>
+
+<style scoped></style>
