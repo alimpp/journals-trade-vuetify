f03addbe17ecf03c52ef949d716c02b28ea7fb92
mahdishoaei <mahdishoaei@gmail.com>
2024-07-03 22:22:49 +0330

    take data from api for dashboard page finished

--- src/api/dashboard.js
+++ src/api/dashboard.js
@@ -1,9 +1,25 @@
-import { JsonParser,ToSting } from "@/utils/string";
+import { JsonParser, ToSting } from "@/utils/string";
 
 export const GetJournalsLengths = () => {
   const journals = localStorage.getItem("journals");
   const result = JsonParser(journals);
+
   const totalLengths = result.length;
-  console.log(total);
-  return result;
+
+  const stoped = result.filter((item) => {
+    return item.state == "Stop";
+  });
+  const stopLengths = stoped.length;
+
+  const fullTarget = result.filter((item) => {
+    return item.state == "Full Target";
+  });
+  const targetLengths = fullTarget.length;
+
+
+  console.log(`total lengths = ${totalLengths}`);
+  console.log(`stop lengths = ${stopLengths} `);
+  console.log(`target lengths = ${targetLengths}`);
+  console.log(fullTarget);
+
 };
